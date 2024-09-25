b76f31b0763a84014a719616a40f42f321d290fd
Logan Connors/PPRClip <151658201+Logan-T-Connors@users.noreply.github.com>
2024-09-25 18:54:49 -0400

    Update main.cpp
    edited code format, refracted code, made code more readable

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ void closingapp(), newcalculation(), errorq1(), errorop1();
 int main() {
 
 	// Asking if the user wants to use the calculator
-	cout << "Would you like to use this calculator?\n";
-	cout << "Please write either yes or no!\n";
+	cout << "\t Would you like to use this calculator?";
+	cout << "\n\t Please write either yes or no: ";
 
 	// Getting user input
 	cin >> userinput;
@@ -25,112 +25,115 @@ int main() {
 	if (userinput == "yes") {
 			
 		// asking what operation the user wants to choose
-		cout << "Choose an operation below.\n";
-		cout << "addition, +\n";
-		cout << "subtraction, -\n";
-		cout << "division, /\n";
-		cout << "multiplication, x\n";
-		cout << "Please type either the whole word or the symbol!\n";
+		cout << "\n\n\t Choose an operation below.";
+		cout << "\n\t addition, +";
+		cout << "\n\t subtraction, -";
+		cout << "\n\t division, /";
+		cout << "\n\t multiplication, x";
+		cout << "\n\n\t Please type either the whole word or the symbol! \n";
 
 		// getting user input for the desired operation
+		cout << "\t ";
 		cin >> useroperation;
 
 		// if statement to see if the users input was valid
 		// if the users input is invalid then gives error statement
 		if (useroperation == "+" || useroperation == "addition") {
 			
-			cout << "You choose Addition\n";
-			cout << "Enter the First Number: ";
+			cout << "\n\n\t You choose Addition";
+			cout << "\n\t Enter the First Number: ";
 			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
+			cout << "\t Enter the Second Number: ";
 			cin >> num2;
 			cout << "\n";
 
 			// num1 + num2 = result
 			result = num1 + num2;
-			cout << "\n";
 
 			// display the answer
-			cout << num1 << " + " << num2 << " = " << result << ".\n";
+			cout << "\t " << num1 << " + " << num2 << " = " << result << ".\n\n\n";
 			newcalculation();
 
 		} else if (useroperation == "-" || useroperation == "subtraction") {
 			
-			cout << "You choose Subtraction\n";
-			cout << "Enter the First Number: ";
+			cout << "\n\n\t You choose Subtraction";
+			cout << "\n\t Enter the First Number: ";
 			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
+			cout << "\t Enter the Second Number: ";
 			cin >> num2;
 			cout << "\n";
 
 			// num1 - num2 = result
 			result = num1 - num2;
-			cout << "\n";
 
 			// display the answer
-			cout << num1 << " - " << num2 << " = " << result << ".\n";
+			cout << "\t " << num1 << " - " << num2 << " = " << result << ". \n\n\n";
 			newcalculation();
 			 
 		} else if (useroperation == "/" || useroperation == "division") {
 
-			cout << "You choose Division\n";
-			cout << "Enter the First Number: ";
+			cout << "\n\n\t You choose Division";
+			cout << "\n\t Enter the First Number: ";
 			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
+			cout << "\t Enter the Second Number: ";
 			cin >> num2;
 			cout << "\n";
 
 			// num1 / num2 = result
 			result = num1 / num2;
-			cout << "\n";
 
 			// display the answer
-			cout << num1 << " / " << num2 << " = " << result << ".\n";
+			cout << "\t " << num1 << " / " << num2 << " = " << result << ". \n\n\n";
 			newcalculation();
 
 		} else if (useroperation == "x" || useroperation == "multiplication") {
 			
-			cout << "You choose Multiplication\n";
-			cout << "Enter the First Number: ";
+			cout << "\n\n\t You choose Multiplication";
+			cout << "\n\t Enter the First Number: ";
 			cin >> num1;
 			cout << "\n";
-			cout << "Enter the Second Number: ";
+			cout << "\t Enter the Second Number: ";
 			cin >> num2;
 			cout << "\n";
 
 			// num1 * num2 = result
 			result = num1 * num2;
-			cout << "\n";
 
 			// display the answer
-			cout << num1 << " x " << num2 << " = " << result << ".\n";
+			cout << "\t " << num1 << " x " << num2 << " = " << result << ".\n\n\n";
 			newcalculation();
 
-		} else {
+		} 
+		
+		else 
+		{
 
 			errorop1();
 
 		}
 
-	} else if (userinput == "no") {
+	} 
+	
+	else if (userinput == "no") 
+	{
 			
 		closingapp();
 
-	} else {
+	} 
+	
+	else 
+	{
 			
 		errorq1();
 
 	}
 }
 
-//
+// funtion that is for closing the program
 void closingapp() {
 
-	cout << "Closing Calculator\n";
-	cout << "Thank you for using my calculator.\n";
+	cout << "\n\n\t Closing Calculator";
+	cout << "\n\t Thank you for using my calculator. \n\t ";
 	this_thread::sleep_for(chrono::seconds(5));
 	exit;
 
@@ -139,132 +142,22 @@ void closingapp() {
 // function that is for when the user is done with a calculation or gets an error statement
 void newcalculation() {
 
-
-	// Asking if the user wants to use the calculator
-	cout << "Would you like to use this calculator?\n";
-	cout << "Please write either yes or no!\n";
-
-	// Getting user input
-	cin >> userinput;
-
-	// if statement checking if user input
-	if (userinput == "yes") {
-
-		// asking what operation the user wants to choose
-		cout << "Choose an operation below.\n";
-		cout << "addition, +\n";
-		cout << "subtraction, -\n";
-		cout << "division, /\n";
-		cout << "multiplication, x\n";
-		cout << "Please type either the whole word or the symbol!\n";
-
-		// getting user input for the desired operation
-		cin >> useroperation;
-
-		// if statement to see if the users input was valid
-		// if the users input is invalid then gives error statement
-		if (useroperation == "+" || useroperation == "addition") {
-
-			cout << "You choose Addition\n";
-			cout << "Enter the First Number: ";
-			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
-			cin >> num2;
-			cout << "\n";
-
-			// num1 + num2 = result
-			result = num1 + num2;
-			cout << "\n";
-
-			// display the answer
-			cout << num1 << " + " << num2 << " = " << result << ".\n";
-			newcalculation();
-
-		} else if (useroperation == "-" || useroperation == "subtraction") {
-
-			cout << "You choose Subtraction\n";
-			cout << "Enter the First Number: ";
-			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
-			cin >> num2;
-			cout << "\n";
-
-			// num1 - num2 = result
-			result = num1 - num2;
-			cout << "\n";
-
-			// display the answer
-			cout << num1 << " - " << num2 << " = " << result << ".\n";
-			newcalculation();
-
-		} else if (useroperation == "/" || useroperation == "division") {
-
-			cout << "You choose Division\n";
-			cout << "Enter the First Number: ";
-			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
-			cin >> num2;
-			cout << "\n";
-
-			// num1 / num2 = result
-			result = num1 / num2;
-			cout << "\n";
-
-			// display the answer
-			cout << num1 << " / " << num2 << " = " << result << ".\n";
-			newcalculation();
-
-		} else if (useroperation == "x" || useroperation == "multiplication") {
-
-			cout << "You choose Multiplication\n";
-			cout << "Enter the First Number: ";
-			cin >> num1;
-			cout << "\n";
-			cout << "Enter the Second Number: ";
-			cin >> num2;
-			cout << "\n";
-
-			// num1 * num2 = result
-			result = num1 * num2;
-			cout << "\n";
-
-			// display the answer
-			cout << num1 << " x " << num2 << " = " << result << ".\n";
-			newcalculation();
-
-		} else {
-
-			errorop1();
-
-		}
-
-	} else if (userinput == "no") {
-
-		closingapp();
-
-	} else {
-
-		errorq1();
-
-	}
+	main();
 
 }
 
-//
+// function for handling if the user types anything other then yes or no
 void errorq1() {
 
-	cout << "Please write either yes or no!\n";
+	cout << "\n\n\t Please write either yes or no! \n";
 	newcalculation();
 
 }
 
-//
+// function for handling if the user types anything other then the word or symbol for the operations
 void errorop1() {
 
-	cout << "Please choose a valid operation!\n";
+	cout << "\n\n\t Please choose a valid operation! \n";
 	newcalculation();
 
 }
