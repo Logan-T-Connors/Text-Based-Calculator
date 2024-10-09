0a2d3a0ddcb998510269cb0cfd88d400deba73b1
Logan Connors/PPRClip <151658201+Logan-T-Connors@users.noreply.github.com>
2024-10-09 13:24:50 -0400

    Update main.cpp
    Removed Whitespace

--- main.cpp
+++ main.cpp
@@ -14,10 +14,8 @@ void closingapp(), errorq1(), errorop1(), divisionBy0();
 
 int main() 
 {
-
 	while (true)
 	{
-
 		// Asking if the user wants to use the calculator
 		cout << "\t Would you like to use this calculator?";
 		cout << "\n\t Please write either yes or no: ";
@@ -28,7 +26,6 @@ int main()
 		// if statement checking if user input
 		if (userinput == "yes") 
 		{
-
 			// asking what operation the user wants to choose
 			cout << "\n\n\t Choose an operation below.";
 			cout << "\n\t addition, +";
@@ -45,7 +42,6 @@ int main()
 			// if the users input is invalid then gives error statement
 			if (useroperation == "+" || useroperation == "addition") 
 			{
-
 				cout << "\n\n\t You choose Addition";
 				cout << "\n\t Enter the First Number: ";
 				cin >> num1;
@@ -58,12 +54,9 @@ int main()
 
 				// display the answer
 				cout << "\t " << num1 << " + " << num2 << " = " << result << ".\n\n\n";
-
 			}
-
 			else if (useroperation == "-" || useroperation == "subtraction") 
 			{
-
 				cout << "\n\n\t You choose Subtraction";
 				cout << "\n\t Enter the First Number: ";
 				cin >> num1;
@@ -76,12 +69,9 @@ int main()
 
 				// display the answer
 				cout << "\t " << num1 << " - " << num2 << " = " << result << ". \n\n\n";
-
 			}
-
 			else if (useroperation == "/" || useroperation == "division") 
 			{
-
 				cout << "\n\n\t You choose Division";
 				cout << "\n\t Enter the First Number: ";
 				cin >> num1;
@@ -92,26 +82,19 @@ int main()
 				// if statement to handle division by 0 
 				if (num2 == 0)
 				{
-
 					divisionBy0();
-
 				}
-
 				else
 				{
-
 					// num1 / num2 = result
 					result = num1 / num2;
 
 					// display the answer
 					cout << "\t " << num1 << " / " << num2 << " = " << result << ". \n\n\n";
-
 				}
 			}
-
 			else if (useroperation == "x" || useroperation == "multiplication") 
 			{
-
 				cout << "\n\n\t You choose Multiplication";
 				cout << "\n\t Enter the First Number: ";
 				cin >> num1;
@@ -127,28 +110,18 @@ int main()
 				cout << "\t " << num1 << " x " << num2 << " = " << result << ".\n\n\n";
 
 			}
-
 			else
 			{
-
 				errorop1();
-
 			}
-
 		}
-
 		else if (userinput == "no")
 		{
-
 			closingapp();
-
 		}
-
 		else
 		{
-
 			errorq1();
-
 		}
 	}
 }
@@ -156,36 +129,26 @@ int main()
 // funtion that is for closing the program
 void closingapp() 
 {
-
 	cout << "\n\n\t Closing Calculator";
 	cout << "\n\t Thank you for using my calculator. \n\t ";
 	this_thread::sleep_for(chrono::seconds(5));
 	exit(0);
-
 }
 
 // function for handling if the user types anything other then yes or no
 void errorq1() 
 {
-
-	cout << "\n\n\t Please write either yes or no! \n";
-	
-
+	cout << "\n\n\t Please write either yes or no! \n";	
 }
 
 // function for handling if the user types anything other then the word or symbol for the operations
 void errorop1() 
 {
-
-	cout << "\n\n\t Please choose a valid operation! \n";
-	
-
+	cout << "\n\n\t Please choose a valid operation! \n";	
 }
 
 void divisionBy0()
 {
-
 	cout << "\n\t\t You are not able to divide by 0";
 	cout << "\n\t\t Please choose a number that is greater then 0";
-
 }
