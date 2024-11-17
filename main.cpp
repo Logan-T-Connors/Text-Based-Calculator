6b75e99e2b19feeecbaf135c0a1685036c989c25
Logan Connors/PPRClip <151658201+Logan-T-Connors@users.noreply.github.com>
2024-11-17 08:02:32 -0500

    Update main.cpp
    fixed a few errors

--- main.cpp
+++ main.cpp
@@ -18,16 +18,16 @@ bool getValidNumber(float& num)
 {
 	while (true)
 	{
-		cin >> num;
+		std::cin >> num;
 		if (cin.fail())
 		{
-			cin.clear(); // Clear the error state
-			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
-			cout << "\n\t The input is not a valid number. Please try again: ";
+			std::cin.clear(); // Clear the error state
+			std::cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
+			std::cout << "\n\t The input is not a valid number. Please try again: ";
 		}
 		else
 		{
-			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input buffer
+			std::cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input buffer
 			return true; // Valid input
 		}
 	}
@@ -38,60 +38,60 @@ int main()
 	while (true)
 	{
 		// Asking if the user wants to use the calculator
-		cout << "\t Would you like to use this calculator?";
-		cout << "\n\t Please write either yes or no: ";
+		std::cout << "\t Would you like to use this calculator?";
+		std::cout << "\n\t Please write either yes or no: ";
 
 		// Getting user input
-		cin >> userinput;
+		std::cin >> userinput;
 
 		// if statement checking if user input
 		if (userinput == "yes")
 		{
 			// asking what operation the user wants to choose
-			cout << "\n\n\t Choose an operation below.";
-			cout << "\n\t addition, +";
-			cout << "\n\t subtraction, -";
-			cout << "\n\t division, /";
-			cout << "\n\t multiplication, x";
-			cout << "\n\n\t Please type either the whole word or the symbol! \n";
+			std::cout << "\n\n\t Choose an operation below.";
+			std::cout << "\n\t addition, +";
+			std::cout << "\n\t subtraction, -";
+			std::cout << "\n\t division, /";
+			std::cout << "\n\t multiplication, x";
+			std::cout << "\n\n\t Please type either the whole word or the symbol! \n";
 
 			// getting user input for the desired operation
-			cout << "\t ";
-			cin >> useroperation;
+			std::cout << "\t ";
+			std::cin >> useroperation;
 
 			// if statement to see if the users input was valid
 			// if the users input is invalid then gives error statement
 			if (useroperation == "+" || useroperation == "addition")
 			{
-				cout << "\n\n\t You choose Addition";
-				cout << "\n\t Enter the First Number: ";
+				std::cout << "\n\n\t You choose Addition";
+				std::cout << "\n\t Enter the First Number: ";
 				getValidNumber(num1); // Use the new function to check if the number is valid
 
-				cout << "\t Enter the Second Number: ";
+				std::cout << "\t Enter the Second Number: ";
 				getValidNumber(num2); // Use the new function to check if the number is valid
 
 				result = num1 + num2;
-				cout << "\t " << num1 << " + " << num2 << " = " << result << ".\n\n\n";
+				std::cout << "\t " << num1 << " + " << num2 << " = " << result << ".\n\n\n";
 			}
 			else if (useroperation == "-" || useroperation == "subtraction")
 			{
-				cout << "\n\n\t You choose Subtraction";
-				cout << "\n\t Enter the First Number: ";
+				std::cout << "\n\n\t You choose Subtraction";
+				std::cout << "\n\t Enter the First Number: ";
 				getValidNumber(num1); // Use the new function to check if the number is valid
 
-				cout << "\t Enter the Second Number: ";
+				std::cout << "\t Enter the Second Number: ";
 				getValidNumber(num2); // Use the new function to check if the number is valid
 
 				result = num1 - num2;
-				cout << "\t " << num1 << " - " << num2 << " = " << result << ".\n\n\n";
+				std::cout << "\t " << num1 << " - " << num2 << " = " << result << ".\n\n\n";
 			}
 			else if (useroperation == "/" || useroperation == "division")
 			{
-				cout << "\n\n\t You choose Division";
-				cout << "\n\t Enter the First Number: ";
+				std::cout << "\n\n\t You choose Division";
+				std::cout << "\n\t Enter the First Number: ";
 				getValidNumber(num1); // Use the new function to check if the number is valid
 
-				cout << "\t Enter the Second Number: ";
+				std::cout << "\t Enter the Second Number: ";
 				getValidNumber(num2); // Use the new function to check if the number is valid
 
 				// if statement to handle division by 0 
@@ -102,20 +102,20 @@ int main()
 				else
 				{
 					result = num1 / num2;
-					cout << "\t " << num1 << " / " << num2 << " = " << result << ".\n\n\n";
+					std::cout << "\t " << num1 << " / " << num2 << " = " << result << ".\n\n\n";
 				}
 			}
 			else if (useroperation == "x" || useroperation == "multiplication")
 			{
-				cout << "\n\n\t You choose Multiplication";
-				cout << "\n\t Enter the First Number: ";
+				std::cout << "\n\n\t You choose Multiplication";
+				std::cout << "\n\t Enter the First Number: ";
 				getValidNumber(num1); // Use the new function to check if the number is valid to check if the number is valid
 
-				cout << "\t Enter the Second Number: ";
+				std::cout << "\t Enter the Second Number: ";
 				getValidNumber(num2); // Use the new function to check if the number is valid
 
 				result = num1 * num2;
-				cout << "\t " << num1 << " x " << num2 << " = " << result << ".\n\n\n";
+				std::cout << "\t " << num1 << " x " << num2 << " = " << result << ".\n\n\n";
 
 			}
 			else
@@ -134,29 +134,29 @@ int main()
 	}
 }
 
-// funtion that is for closing the program
+// function that is for closing the program
 void closingapp()
 {
-	cout << "\n\n\t Closing Calculator";
-	cout << "\n\t Thank you for using my calculator. \n\t ";
-	this_thread::sleep_for(chrono::seconds(5));
-	return
+	std::cout << "\n\n\t Closing Calculator";
+	std::cout << "\n\t Thank you for using my calculator. \n\t ";
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	return;
 }
 
 // function for handling if the user types anything other then yes or no
 void errorq1()
 {
-	cout << "\n\n\t Please write either yes or no! \n";
+	std::cout << "\n\n\t Please write either yes or no! \n";
 }
 
 // function for handling if the user types anything other then the word or symbol for the operations
 void errorop1()
 {
-	cout << "\n\n\t Please choose a valid operation! \n";
+	std::cout << "\n\n\t Please choose a valid operation! \n";
 }
 
 void divisionBy0()
 {
-	cout << "\n\t\t You are not able to divide by 0";
-	cout << "\n\t\t Please choose a number that is greater then 0";
+	std::cout << "\n\t\t You are not able to divide by 0";
+	std::cout << "\n\t\t Please choose a number that is greater then 0";
 }
